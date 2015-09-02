7b10a5a27a32803ef2129f042362767d469f76ba
Syam  S Pillai <syam.sp0965@gmail.com>
2015-09-02 13:16:29 +0530

    mobile view menu and notification completed

--- js/script.js
+++ js/script.js
@@ -12,9 +12,23 @@ function show_menu() {
 }
 function hide() {
     var menu = document.getElementById("hidden_menu");
+    var notif = document.getElementById("hidden_notif");
     var back = document.getElementById("back");
     if(back.style.display == "block"){
         menu.style.left = "-250px";
+        back.style.display = "none";
+        notif.style.right = "-250px";
+    }
+}
+function show_notification(){
+    var notif = document.getElementById("hidden_notif");
+    var back = document.getElementById("back");
+    if(notif.style.right >= "-250px"){
+        notif.style.right = "0";
+        back.style.display = "block";
+    }
+    else{
+        notif.style.right = "-250px";
         back.style.display = "none";
     }
 }
